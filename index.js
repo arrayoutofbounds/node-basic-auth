4b31ac91a5b499295c4aec28eeaa9335752ade5d
anmoldesai <anmol.desai@lendi.com.au>
2019-03-14 17:07:53 +1100

    allow body parse json

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 8888;
+
+app.use(bodyParser.json());
 
 app.get("/status", (req, res) => {
     const localtime = (new Date()).toLocaleTimeString();
@@ -11,6 +14,11 @@ app.get("/status", (req, res) => {
 
 app.get("*", (req, res) => {
     res.sendStatus(404);
+})
+
+app.post('/login', (req,res) => {
+    const user = req.body.username;
+    res.status(200).send(`you logged in with ${user}`);
 })
 
 app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
