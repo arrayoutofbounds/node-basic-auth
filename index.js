61674f5484374b98c63dc8546104a0f4af66b810
anmoldesai <anmol.desai@lendi.com.au>
2019-03-15 09:13:18 +1100

    secure the routes using jwt

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const jwt = require("jsonwebtoken");
-const cors = require("cors")
+const cors = require("cors");
+const expressjwt = require("express-jwt");
 
 const app = express();
 const PORT = process.env.PORT || 8888;
@@ -14,15 +15,27 @@ const users = [
 app.use(bodyParser.json());
 app.use(cors());
 
+const jwtCheck = expressjwt({
+    secret: "secret"
+})
+
 app.get("/status", (req, res) => {
     const localtime = (new Date()).toLocaleTimeString();
 
     res.status(200).send(`time is ${localtime}`);
 })
 
+app.get("/resource", (req, res) => {
+    res.status(200).send("Public resource");
+})
+
+app.get("/resource/secret", jwtCheck, (req, res) => {
+    res.status(200).send("Secret resource");
+})
+
 app.get("*", (req, res) => {
     res.sendStatus(404);
-})
+});
 
 app.post('/login', (req,res) => {
     if(!req.body.username || !req.body.password){
